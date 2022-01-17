af3c8f800adfef51b2f9537704bb4e597d1ed6b0
angelnovoa <angel@angelnovoa.com>
2022-01-17 16:10:26 +0100

    subida javadoc

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"angelnovoamartinez53357585t"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"angelnovoamartinez53357585t","l":"AngelNovoaMartinez_53357585T"}];updateSearchResults();
